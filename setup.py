60c071af69b3517bd1c50ac226cd04dac836c91a
Rohit Kushwaha <66631661+rk101294@users.noreply.github.com>
2025-03-31 19:32:55 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="src",
-    version="0.0.1",
-    author="Rohit Kushwaha",
-    author_email="kush.rohit795@gmail.com",
-    packages=find_packages()
-)
